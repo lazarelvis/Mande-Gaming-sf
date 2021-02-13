e2934d76f88bf321f7b673ff3498b79e779a0666
Popa Mihai <popamihai199@yahoo.com>
2021-02-13 08:59:03 -1000

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import "./App.css";
 function App() {
   return (
+    <>
     <div>
       <h1>Hello Mande!</h1>
     </div>
+    <div>
+      <h2>Hello Elvis/Mihai</h2>
+    </div>
+    </>
   );
 }
 
