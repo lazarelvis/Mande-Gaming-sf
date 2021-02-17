193f011ced336079c579dd5800f5f1e146497151
Elvis lazar <lazarelvis15@gmail.com>
2021-02-17 21:42:18 +0200

    routes and layout added

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import "./App.css";
+function Home() {
+  return (
+    <>
+    <div>
+      <h1>Hello Mande!</h1>
+    </div>
+    <div>
+      <h2>Hello Elvis/Mihai</h2>
+    </div>
+    </>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Routes from "./routes";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(
+<BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    , document.getElementById("root"));
