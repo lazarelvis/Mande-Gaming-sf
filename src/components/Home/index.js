86050c229e354277bb6a60cffde804e94281abf1
Popa Mihai <popamihai199@yahoo.com>
2021-02-18 22:36:14 -1000

    All the layout is done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,12 +2,7 @@ import "./App.css";
 function Home() {
   return (
     <>
-    <div>
-      <h1>Hello Mande!</h1>
-    </div>
-    <div>
-      <h2>Hello Elvis/Mihai</h2>
-    </div>
+  
     </>
   );
 }
