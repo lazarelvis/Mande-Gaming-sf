ed4623e25b78c2ecb868d310af6757875794bad3
Popa Mihai <popamihai199@yahoo.com>
2021-02-20 06:10:23 -1000

    navbar, and join button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Header = () => {
     return ( 
-        <div>
-            header
+        <>
+         <div class="navbar-button">
+            <a href="#">Join Us</a>
         </div>
+        <div class="navbar">
+            <ul>
+                <li><a href="#">Leaderboard</a></li>
+                <li><a href="#">Gaming</a></li>
+                <li><a href="#">Home</a></li>
+            </ul>
+        </div>
+        </>
      );
 }
  
