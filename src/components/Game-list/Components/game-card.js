a192026bbe2bf378e6ad60b07c6042459b2eb4ee
Popa Mihai <popamihai199@yahoo.com>
2021-04-07 20:33:28 +0300

    adding data-base links  to the games card

--- src/components/Game-list/Components/game-card.js
+++ src/components/Game-list/Components/game-card.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import gamepic from '../../../Resources/images/pig3.jpg';
+import { Link } from 'react-router-dom';
 
 const GameCard = (props) => {
     return ( 
+    <Link to={`/game/${props.nume}`}>
     <div className="card-game">
         <img src={gamepic} />
-        <button type="button">{props.nume}</button>
+        <button type="button">Play Game</button>
     </div>
+    </Link>
      );
 }
  
--- src/components/Game-list/Components/index.js
+++ src/components/Game-list/Components/index.js
@@ -19,12 +19,6 @@ const GameList = ({fetchGames, games, users}) => {
     return (  
         <>
         <div className="grid">
-           {/* <GameCard  nume="Play Now" />
-           <GameCard nume ="Play Later"  />
-           <GameCard nume ="Plaido" />
-           <GameCard nume ="Elvis" />
-           <GameCard nume ="Mihai" />
-           <GameCard nume ="I love react" /> */}
            {showGameCards()}
         </div>
         </>
