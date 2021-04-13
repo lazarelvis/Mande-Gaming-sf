e00789b6fbbb6f339311ca8eb6264dae54c610e5
Popa Mihai <popamihai199@yahoo.com>
2021-04-13 16:02:18 +0300

    Some updates

--- src/components/Leaderboard/headrow.js
+++ src/components/Leaderboard/headrow.js
@@ -2,11 +2,13 @@ import React from 'react';
 const HeadRow = () => {
     return ( 
         <>
-            <tr>
-                <th style={{textAlign: 'left'}}>User</th>
-                <th style={{textAlign: 'center',paddingLeft:0,paddingRight:90}}>Game</th>
-                <th style={{textAlign: 'right'}}>Score</th>
-            </tr>
+            <thead>
+                <tr>
+                    <th style={{textAlign: 'left'}}>User</th>
+                    <th style={{textAlign: 'center',paddingLeft:0,paddingRight:90}}>Game</th>
+                    <th style={{textAlign: 'right'}}>Score</th>
+                </tr>
+            </thead>
         </>
      );
 }
