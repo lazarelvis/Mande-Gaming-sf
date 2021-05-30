537377f66c371e599c1a81278b63c0eb82b5d2fc
Popa Mihai <popamihai199@yahoo.com>
2021-05-30 19:45:17 +0300

    iframe work

--- src/components/Game/Components/Game.js
+++ src/components/Game/Components/Game.js
@@ -135,16 +135,12 @@ const Game = ({
 
   let executed = false;
   if (score != 0) {
-<<<<<<< HEAD
-    if (localStorage.getItem("username-mande-gaming") != undefined) {
-=======
     if (
-      localStorage.getItem('username-mande-gaming') != undefined &&
+      localStorage.getItem("username-mande-gaming") != undefined &&
       scoreByUsername.length == 0
     ) {
->>>>>>> 65c5745dcf1bf967d17b225e6e7bc2a213b1097f
       createScore(userNameToCreate);
-      fetchScoreByUsername(localStorage.getItem('username-mande-gaming'));
+      fetchScoreByUsername(localStorage.getItem("username-mande-gaming"));
     } else if (score != 0) {
       // scoreByUsername[0].score.filter((game) => {});
       if (!isEmpty(scoreByUsername)) {
@@ -207,7 +203,10 @@ const Game = ({
       return <Unity unityContext={unityContext} />;
     } else {
       return (
-        <iframe src="https://stackoverflow.com/users/7159290/prateek-agrawal?tab=profile#user-card" />
+        <iframe
+          src={`${gameById[0].frame}`}
+          style={{ width: "1200px", height: "1200px" }}
+        ></iframe>
       );
     }
   };
